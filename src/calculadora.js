c9797f3d053a8cb36d25eb53922dee66dbcc32ee
auriceliof <auricelio.suporte@hotmail.com>
2024-09-14 16:10:42 +0000

    Add function power in calculate.js

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,39 @@
-    // Função de adição     function add(a, b) {         return a + b; 
-    }    
+    // Função de adição 
+        function add(a, b) {
+           return a + b; 
+         }
+
+    // Função de subtrao
+        function subtract(a, b) {
+          return a - b;
+        }
+
+    // Função de multiplicação
+        function multiply(a, b) {
+          return a * b;
+        }    
  
-    // Exportar as funções     module.exports = { 
+
+    // Função de divisão
+        function divide(a, b) {
+          if (b === 0) {
+            throw new Error("Divisão por zero não é permitida.");
+            }
+          return a / b;
+         }
+
+    // Função de potenciação
+     function power(base, exponent) {   
+      return Math.pow(base, exponent); 
+    } 
+
+
+    // Exportar as funçes    
+       module.exports = { 
         add 
         subtract 
         multiply 
+        divide
+        power 
     };     
 
