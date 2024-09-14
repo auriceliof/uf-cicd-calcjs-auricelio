edce4132160a22ce83106531c5a79f2523eea095
auriceliof <auricelio.suporte@hotmail.com>
2024-09-14 15:41:05 +0000

    Created project

--- src/calculadora.js
+++
@@ -1,8 +0,0 @@
-    // Função de adição     function add(a, b) {         return a + b; 
-    }    
- 
-    // Exportar as funções     module.exports = { 
-        add
-        subtract  
-    };     
-
